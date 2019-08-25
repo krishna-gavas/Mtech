8ec0ddbaedd5436de97541eff5218e4ad8cd90bc
krishna-gavas <krishna.gavas@gmail.com>
2019-08-25 20:45:13 +0530

    CommonCourse() added

--- SSL_Lab/1D/DAG.java
+++ SSL_Lab/1D/DAG.java
@@ -74,43 +74,91 @@ class DAG{
 
 
         Stack stack = new Stack();
-         
+        System.out.println("Topological sorting of Courses: "); 
         topologicalSort(in,out,stack,visited);
-
-        while(stack.empty() == false)
-                System.out.print(stack.pop() + " ");
 
         System.out.println();
     }
+
     
     static void RegisterCourse(String name, String course){
         
-        List<String> list1 = prerequisite.get(course); 
-        if(list1 != null){
-            Integer flag = 0;
-            for(int j=0; j<list1.size(); j++){
-                String str = list1.get(j);
-                if(Register.get(str).contains(name)){
-                    //Nothing Here
+        if(prerequisite.containsKey(course)){
+            List<String> list1 = prerequisite.get(course); 
+            if(list1 != null){
+                Integer flag = 0;
+                for(int j=0; j<list1.size(); j++){
+                    String str = list1.get(j);
+                    if(Register.get(str).contains(name)){
+                        //Nothing Here
+                    }
+                    else{
+                        flag = 1;
+                        break;
+                    }
                 }
-                else{
-                    flag = 1;
-                    break;
+                if(flag == 0){
+                    Register.get(course).add(name);
+                    System.out.println("Registered Successfully");
                 }
+                else
+                    System.out.println("Registration Failed");
             }
-            if(flag == 0){
+            else{
+                
                 Register.get(course).add(name);
                 System.out.println("Registered Successfully");
             }
-            else
-                System.out.println("Registration Failed");
         }
         else{
-            
-            Register.get(course).add(name);
-            System.out.println("Registered Successfully");
+            System.out.println("Course Does not Exist");
         }
+        
     }
+
+
+    static void CommonCourse(String name1,String name2){
+        List<String> list = new ArrayList();
+        List<String> list1 = new ArrayList();
+        
+        for (String i: Register.keySet()){
+            if((Register.get(i).contains(name1)) && (Register.get(i).contains(name2))){
+                list.add(i);
+                System.out.println(i + " ");
+            }
+        }
+
+        for (String i: prerequisite.keySet()){
+            List<String> list2 = prerequisite.get(i); 
+            if(list2 != null){
+                Integer flag = 0;
+                for (int j = 0; j < list2.size(); j++){
+                    if(list.contains(list2.get(j))){
+                        //Nothing Here
+                    }
+                    else{
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 0){
+                    if(!list.contains(i))
+                        list1.add(i);
+                }
+            }
+            else{
+                if(!list.contains(i))
+                    list1.add(i);
+            }
+        }
+
+        System.out.print("Common Courses are: ");
+        for (int i = 0; i < list1.size(); i++)
+            System.out.print(list1.get(i) + " ");
+
+        System.out.println();
+    }
+
 
     public static void main(String args[])throws IOException
     {
@@ -150,21 +198,38 @@ class DAG{
 
         //int n = graph.size(); 
         findInOutDegree(V); 
-        Integer choice = 1;
+        Integer choice = 4;
 
-        while(choice == 1){
-            
-            System.out.print("Enter Student Name: ");
-            String name = in.readLine();
-    
-            System.out.print("Enter Course Name: ");
-            String course = in.readLine();
-    
-            RegisterCourse(name,course);
+        while(choice != 3){
 
-            System.out.print("Press 1 to continue, 2 to exit: ");
+            System.out.print("1.Register\t2.Common Course\t3.Exit: ");
             choice = Integer.parseInt(in.readLine());
             System.out.println();
+
+            switch(choice){
+                case 1: System.out.print("Enter Student Name: ");
+                        String name = in.readLine();
+                
+                        System.out.print("Enter Course Name: ");
+                        String course = in.readLine();
+                
+                        RegisterCourse(name,course);
+                        break;
+
+                case 2: System.out.print("Enter Student 1 Name: ");
+                        String name1 = in.readLine();
+                
+                        System.out.print("Enter Student 2 Name: ");
+                        String name2 = in.readLine();
+
+                        CommonCourse(name1,name2);
+                        break;
+
+                case 3: break;
+
+                default: break;
+            }
+            
         }
         
 
