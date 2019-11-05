92088549ece4c7b51157b26a55abad78200b0fce
krishna-gavas <krishna.gavas@gmail.com>
2019-11-05 21:27:18 +0530

    Added 3B with 1 complete

--- Sem1/SSL_Lab/3A/script4.sh
+++ Sem1/SSL_Lab/3A/script4.sh
@@ -1,3 +1,32 @@
-# awk 'NR==FNR && /^[^#]/{a[FNR]=$0;next}$0==a[FNR] && /^[^#]/{print}' file2.txt file1.txt
+words=$(cat file2.txt  | wc -w)
 
-awk 'FNR==NR{ A[$1]++; next}{ print $1 , A[$1]+0 }' file2.txt file1.txt
+awk -v total="$words" '
+    BEGIN{
+        FS=" " ; 
+        count=0
+    }
+
+    FNR==NR && /^[^#]/{
+        arr[FNR]=$0;
+        next
+    }
+    {
+        if ($0==arr[FNR]){
+            count=count+NF
+        }
+        else{
+
+        }
+    }
+    END{
+        if(count/total > 0.8){
+            print "There is Plagarism"
+        }
+        else{
+            print "No plagarism"
+        }
+    }
+
+' file1.txt file2.txt
+
+# output=`expr $num1 + $num2`
---
+++ Sem1/SSL_Lab/3B/script1.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+sub sum_sub{   
+    $n = scalar(@_);
+    $sum = 0;
+    my @list = @_;
+
+    foreach $item (@list) {
+        $sum += $item;
+    }
+
+    return $sum;
+}
+
+sub avg_sub{
+    my ($n, $sum) = (@_);
+
+    $avg = $sum / $n;
+
+    return $avg;
+}
+
+print "Enter the numbers: "; 
+print "<Ctrl>-D to Terminate \n"; 
+@nums = <STDIN>; 
+  
+# Removes new line appended at the end of every input 
+chomp @nums; 
+  
+$sum = sum_sub(@nums);
+print "Sum of the given numbers : $sum\n";
+
+$avg = avg_sub(scalar(@nums), $sum);         # scalar(@nums) get total number of elements.
+print "Average of the given numbers : $avg\n";
